8530032bd591a179a407e644f956f44475414d84
Leandro Rodrigues <lj251004@gmail.com>
2024-12-28 19:37:01 -0300

    Add initial components and utilities for UI and document management: shadcn, tiptap, zustand

---
+++ src/app/documents/[documentId]/page.tsx
@@ -0,0 +1,15 @@
+import { Editor } from "./editor";
+import { Toolbar } from "./toolbar";
+
+interface DocumentIdPageProps {
+  params: Promise<{ documentId: string }>;
+}
+
+export default async function DocumentIdPage({}: DocumentIdPageProps) {
+  return (
+    <div className="min-h-screen bg-[#FAFBFD]">
+      <Toolbar />
+      <Editor />
+    </div>
+  );
+}
